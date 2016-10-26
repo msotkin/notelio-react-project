967bbd8d5e1e26dcc817967aeb35f346bd35de44
Michael Sotkin <michael.sotkin@gmail.com>
2016-10-26 11:45:43 -0700

    added ability to create more cards with drag and drop functionality

--- app/app.jsx
+++ app/app.jsx
@@ -2,12 +2,51 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
 import Notecard from './components/Notecard';
+import Navbar from './components/Navbar';
+import styles from './scss/application.scss';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cards: [{id: 0, x: 0, y:0}]
+    };
+    this.onMove = this.onMove.bind(this);
+    this.makeCard = this.makeCard.bind(this);
+    this.cloneState = this.cloneState.bind(this);
+  }
+
+onMove(e, id) {
+  console.log(id);
+  let clone = this.cloneState();
+  let coordsObj = {id: id, x: clone.cards[id].x + e.dx, y: clone.cards[id].y + e.dy}
+  clone.cards[id] = coordsObj;
+  this.setState(clone);
+}
+
+cloneState() {
+  return Object.assign({}, this.state);
+}
+
+makeCard() {
+  let clone = this.cloneState();
+  clone.cards.push({id: clone.cards.length, x: 0, y:0});
+  this.setState(clone);
+  console.log(this.state);
+}
+
+
   render() {
+    let cardArr = [];
+    this.state.cards.forEach(card => {
+      console.log(card.id);
+      cardArr.push(<Notecard key = {card.id} id = {card.id} coords={card} onMove= {(e) => {this.onMove(e, card.id)}} />)
+    })
     return (
     <div>
-      <Notecard />
+      <Navbar makeCard = {this.makeCard}/>
+      
+      { cardArr }
     </div>
     )
   }
--- app/components/Notecard.jsx
+++ app/components/Notecard.jsx
@@ -1,11 +1,38 @@
 import React, { PropTypes } from 'react';
+import { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import interact from 'interact.js';
 
-const Notecard = () => {
-    return (
-        <div>
-            <textarea className="notecard" rows="25" cols="80"></textarea>
-        </div>
-    )
+class Notecard extends Component {
+
+    componentDidMount() {
+        let gridTarget = interact.createSnapGrid({
+            x: 0, 
+            y: 300, 
+            range: 50
+        });
+        interact(ReactDOM.findDOMNode(this))
+            .draggable({
+                onmove: this.props.onMove,
+                snap: {
+                    targets: [gridTarget],
+                    endOnly: true
+                }
+            });
+    }
+
+    render() {
+        return (
+            <textarea className="notecard" maxLength="200" style={this.style()} ></textarea>
+        )
+    }
+
+    style() {
+        return {
+            transform: 'translate('+ this.props.coords.x +'px, '+this.props.coords.y+'px)'
+        }
+    }
 }
 
 export default Notecard;
